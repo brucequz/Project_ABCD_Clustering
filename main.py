f854f2a08dd4f492f3145864eb3d5468ee8df533
UCLAbrucequ <brucequ@ucla.edu>
2023-03-07 17:37:29 -0800

    chore: initialize main function

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+
+def main():
+    # Data Preprocessing
+    print("Processing Data ... ")
+    
+
+    # Model Fitting
+    print("Fitting Models ... ")
+
+
+    return int(x)
+
+if __name__ == "__main__":
+    main("a")
